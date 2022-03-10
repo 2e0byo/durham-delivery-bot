c126358ee682b8677f445f812f43a2e7146bf53c
John Maximilian <2e0byo@gmail.com>
2022-03-10 22:43:00 +0000

    test: categorise.

--- tests/test_links.py
+++ tests/test_links.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import pytest
 
 from durham_delivery_bot.bot import get_reserve_url, Chrome
 from durham_delivery_bot.cart import get_permalinks, parse_records
+from durham_delivery_bot import categorise
 
 
 def test_get_permalinks(data_regression):
@@ -20,3 +22,15 @@ def test_reserve_urls(data_regression):
 def test_parse_records(data_regression):
     records = parse_records(Path(__file__).parent / "books.html")
     data_regression.check(records)
+@pytest.mark.parametrize(
+    "in_person",
+    [
+        ["St John's", "Bryson"],
+        ["John's"],
+        ["Ushaw"],
+    ],
+)
+def test_categorise(in_person, data_regression):
+    records = parse_records(Path(__file__).parent / "books.html")
+    collect, reserve = categorise(records, in_person)
+    data_regression.check((collect, reserve))
