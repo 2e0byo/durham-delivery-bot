4226e12d5d0470204ecbfd44f24565827b5f5eab
John Maximilian <2e0byo@gmail.com>
2022-03-10 22:45:47 +0000

    tests: fixups.

--- tests/test_links.py
+++ tests/test_links.py
@@ -15,13 +15,15 @@ def test_reserve_urls(data_regression):
     permalinks = get_permalinks(Path(__file__).parent / "books.html")
     username = "username"
     password = "password"
-    urls = sorted([get_reserve_url(x, driver, username, password) for x in permalinks])
+    urls = sorted([get_reserve_url(x, username, password) for x in permalinks])
     data_regression.check(urls)
 
 
 def test_parse_records(data_regression):
     records = parse_records(Path(__file__).parent / "books.html")
     data_regression.check(records)
+
+
 @pytest.mark.parametrize(
     "in_person",
     [
